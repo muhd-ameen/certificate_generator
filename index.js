a9e6f33d9abd51cef889732b2c11466766031931
AbdulSamad <smd2k15@gmail.com>
2021-12-30 15:29:12 +0400

    center align text drawn on pdf

--- index.js
+++ index.js
@@ -29,17 +29,25 @@ const generatePDF = async (name) => {
   const fontBytes = await fetch("Sanchez-Regular.ttf").then((res) =>
   res.arrayBuffer()
 );
+
+  // Set font size
+  const textSize = 20;
+
   // Embed our custom font in the document
   const SanChezFont  = await pdfDoc.embedFont(fontBytes);
    // Get the first page of the document
    const pages = pdfDoc.getPages();
    const firstPage = pages[0];
- 
+
+   // Calculations for center aligning the text
+   const { width, height } = firstPage.getSize()
+   const textWidth = SanChezFont.widthOfTextAtSize(name, textSize);
+   const textHeight = SanChezFont.heightAtSize(textSize);
    // Draw a string of text diagonally across the first page
    firstPage.drawText(name, {
-     x: 290,
-     y: 270,
-     size: 20,
+     x: width / 2 - textWidth / 2 - 28,
+     y: height / 2 - 32,
+     size: textSize,
      font: SanChezFont ,
      color: rgb(0.0, 0.0, 0.0),
    });
